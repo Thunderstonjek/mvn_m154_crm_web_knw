14e4ef0a2cd6d210dd66799e1745ceaa5f873fb3
Paul Stonjek <paul.stonjek@gibmit.ch>
2019-06-21 15:18:20 +0200

    hopefully fixed now

--- src/test/java/ch/ilv/crm/web/test/CrmAppTest.java
+++ src/test/java/ch/ilv/crm/web/test/CrmAppTest.java
@@ -2,37 +2,41 @@ package ch.ilv.crm.web.test;
 
 import static org.junit.Assert.assertTrue;
 
-import org.openqa.selenium.By;
+import org.junit.Before;
+import org.junit.Test;
+// import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import junit.framework.*;
-
 public class CrmAppTest {
-
-	public static void main(String[] args) {
+	
+	@Before
+	public void setup() {
 		// Configure firefox driver
 		System.setProperty("webdriver.gecko.driver", "D:\\DevTools\\geckodriver.exe");
+	}
+	
+	@Test
+	public void test() {
 		// Start firefox driver
 		WebDriver driver = new FirefoxDriver();
-		
-		// Start website
+        //Launch the Online Store Website
 		driver.get("http://localhost:7070");
-
-		// Use website
+		
+		// start testing
 		String title = driver.getTitle();
 		assertTrue(title.contains("CRM App"));
-
-		// Wait for 5 Sec
+		// driver.findElement(By.xpath("//link[@linktext='Manage Customers']")).click();
+		// driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Linux");
+		// driver.findElement(By.xpath("//input[@name='q']")).submit();
+		
+		//Wait for 5 Sec
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		// Close driver
-		driver.quit();
+        // Close the driver
+        driver.quit();
 	}
-
 }
